4ba1e44fdba1740edb4a29684669344a89b86582
Maria Gamzina <gamzinamaria@gmail.com>
2024-06-29 23:42:50 +0300

    Task1 success

--- src/test/java/ru/netology/WebServiceTest.java
+++ src/test/java/ru/netology/WebServiceTest.java
@@ -1,25 +1,34 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WebServiceTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
-        // WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
     }
+
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -29,9 +38,18 @@ public class WebServiceTest {
     }
 
     @Test
-    void shouldTest() {
-        driver.get("http://0.0.0.0:9999");
-        //Thread.sleep(5000);
+    void shouldTest() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Мария Гамзина");
+        elements.get(1).sendKeys("+79260000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+        Thread.sleep(5000);
+
 
     }
 }
