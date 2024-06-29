9f66d71fc70b6884e9cf1d1b816e2ee400d32997
Maria Gamzina <gamzinamaria@gmail.com>
2024-06-29 23:45:33 +0300

    Task1 fail

--- src/test/java/ru/netology/WebServiceTest.java
+++ src/test/java/ru/netology/WebServiceTest.java
@@ -43,7 +43,7 @@ public class WebServiceTest {
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Мария Гамзина");
         elements.get(1).sendKeys("+79260000000");
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("checkbox__bo")).click();
         driver.findElement(By.className("button")).click();
         String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
